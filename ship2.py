d7c2e2dd407889a5b5db6f869124c68b516b38f2
Edupu Samuel <samedupu@gmail.com>
2023-11-18 19:30:24 +0300

    added 'moving left and right py pressing left and right keys respectively' functionality

---
+++ ship2.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Ship2:
+    """A class to draw the character at the center of the screen"""
+    def __init__(self, ai_game):
+        """Initialize the ship and set it's starting position"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('images/rocket.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.center = self.screen_rect.center
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+        
