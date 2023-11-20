40beb2f7663863d59b3e329cb7a46e872ab23fac
Edupu Samuel <samedupu@gmail.com>
2023-11-20 15:38:22 +0300

    updated the main loop and created bullets

--- settings.py
+++ settings.py
@@ -7,6 +7,14 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (123, 123, 123)
-
+        
         # Ship settings
         self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+
